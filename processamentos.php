c2f065640e5d2e416c2ad02fce2e9e0d514494ac
Wesley Fernandes <86626685+Wess11@users.noreply.github.com>
2022-03-31 23:50:10 -0300

    Update processamentos.php

--- processamentos.php
+++ processamentos.php
@@ -1,26 +1,26 @@
 <?php
 
 $filename = "cadastros.txt";
-if(!file_exists("cadastros.txt")){
-    $handle = fopen("cadastros.txt", "w");
+if(!file_exists("cadastros.txt")){                      //Caso nao exista criar arquivo
+    $handle = fopen("cadastros.txt", "w");              //Criar cadastro com opção de escrita
 } else {
-    $handle = fopen("cadastros.txt", "a");
+    $handle = fopen("cadastros.txt", "a");              //Criar cadastro com opção de escrita, se nao existir criar arquivo
 }
 
-fwrite($handle, $_POST['Nome']);
-fwrite($handle, $_POST['Sobrenome']);
-fwrite($handle, $_POST['Cidade do Aluno']);
-fwrite($handle, $_POST['Data de Nascimento do aluno']);
-fflush($handle);
-fclose($handle);
+fwrite($handle, $_POST['Nome']);                        //Inserir o arquivo nome
+fwrite($handle, $_POST['Sobrenome']);                   //Inserir o arquivo sobrenome
+fwrite($handle, $_POST['Cidade do Aluno']);             //Inserir o arquivo Cidade do Aluno
+fwrite($handle, $_POST['Data de Nascimento do aluno']); //Inserir o arquivo Data de nascimento
+fflush($handle);                                        //Insere os dados no arquivo
+fclose($handle);                                        //Fecha o aqrquivo depois da inserção
 
-$handle = fopen( "cadastros.txt", "r");
+$handle = fopen( "cadastros.txt", "r");                 //Abre o arquivo para somente leitura
 while (!feof($handle)) {
         $line = fgets($handle);
         echo $line ."<br>";
     }
 fclose($handle);
-header("location: welcome.php");
+header("location: welcome.php");                        //Retorna para a pagina inicial(bem vindo)
     exit;
 
 ?>
